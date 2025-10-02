b27ad6bde56591f1e716a4717c59bd2a99fe55b6
Kevin Delfour <kevin.delfour@jobo-ci.com>
2025-10-02 10:12:13 +0200

    :sparkle: Change from hybrid to static mode. Fix Node version to 20.x

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,10 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.cto-de-lyon.fr",
   integrations: [tailwind(), sitemap()],
-  output: "hybrid",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    maxDuration: 8,
-  }),
+  output: "static",
 });
